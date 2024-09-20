9d5c20c311306c92707a37bddc4f49eda907b8f7
hassan03042 <muhammadhassan6948@gmail.com>
2024-09-20 10:56:10 +0500

    color changed

--- src/components/CategoryChip.jsx
+++ src/components/CategoryChip.jsx
@@ -7,7 +7,7 @@ function CategoryChip({ category, isChosen, onClick }) {
           isChosen ? "bg-orange-400 text-white" : "bg-white text-black"
         } p-2 
           cursor-pointer
-          hover:bg-orange-100
+          hover:bg-orange-200
           border-orange-400 border px-4 rounded-md`}
       >
         <h1>{name}</h1>
